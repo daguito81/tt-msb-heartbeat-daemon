700c59029eeb05b6748697c8dcffc5f09f64e575
Dago Romer <dagoromer85@gmail.com>
2021-01-24 21:28:47 +0100

    removed log to file

--- main.go
+++ main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"context"
 	"encoding/json"
-	"io"
-	"os"
 	"sync"
 	"time"
 
@@ -27,13 +25,7 @@ var counter = 0
 
 func main() {
 	log.SetLevel(log.InfoLevel)
-	file, err := os.OpenFile("sbdaemon.log", os.O_CREATE|os.O_WRONLY, 0666)
-	if err != nil {
-		log.Warn("Failed to log to file, using default stderr")
-	} else {
-		mw := io.MultiWriter(os.Stdout, file)
-		log.SetOutput(mw)
-	}
+
 	// Async Setup
 	clientsFTP := make(chan *servicebus.Subscription, nw)
 	clientsMQTT := make(chan *servicebus.Subscription, nw)
